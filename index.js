e93d1fb8d958519b40619c42678987376fffd6d7
DvHuy <ahuyahihi4@gmail.com>
2024-10-09 15:22:13 +0700

    Initial Backend Of Project

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv"
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectToMongoDB from './src/db/connectToMongoDB.js';
+
+dotenv.config();
+const PORT = process.env.PORT || 5000
+const app = express();
+
+//test api
+app.get("/", (req, res)=>{
+    res.send("api working")
+})
+
+// middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
