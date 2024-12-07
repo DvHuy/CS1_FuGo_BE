38c6c61f84e598858bd6cda6da3e8cf4904df290
DvHuy <ahuyahihi4@gmail.com>
2024-12-07 09:59:00 +0700

    SendMail for User function, Notification Model

--- src/models/JobApplication.js
+++ src/models/JobApplication.js
@@ -1,23 +1,24 @@
 import mongoose from "mongoose";
-import {Schema} from "mongoose";
+import { Schema } from "mongoose";
 
-const jobApplicationSchema = new Schema({
+const jobApplicationSchema = new Schema(
+  {
     job_cv_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "JobCV",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "JobCV",
+      required: true,
     },
-    status:{
-        type: String,
-        enum: ["Đang duyệt", "Chấp nhận", "Từ chối"],
-        default: "Đang duyệt"
+    status: {
+      type: String,
+      enum: ["Đang duyệt", "Chấp nhận", "Từ chối"],
+      default: "Đang duyệt",
     },
-    applied_at: {
-        type: Date,
-        default: Date.now,
-    }
-})
+  },
+  {
+    timestamps: true, 
+  }
+);
 
-const JobApplication = mongoose.model("jobApplication", jobApplicationSchema)
+const JobApplication = mongoose.model("jobApplication", jobApplicationSchema);
 
 export default JobApplication;
